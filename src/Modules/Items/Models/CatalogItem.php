eff93c4a8e1c7707c66ea6c6c16e5e01158f28a0
mohannad <mohannad.zaghmouri@olahub.com>
2020-10-03 15:36:46 +0300

    fix

--- src/Modules/Items/Models/CatalogItem.php
+++ src/Modules/Items/Models/CatalogItem.php
@@ -251,31 +251,28 @@ class CatalogItem extends Model
         $find = CatalogItem::where(function ($query) {
             $query->whereNull('parent_item_id');
             $query->orWhere('parent_item_id', '0');
-        })->where(function ($qe1) use ($whereQuery,$itemQuery) {
-            $qe1->where(function ($q) use ($whereQuery) {
+        })->where(function ($q) use ($whereQuery) {
             //occasions
-                $q->whereHas("occasionSync", function ($q1) use ($whereQuery) {
-                    $q1->whereRaw($whereQuery);
-                });
-                // interests
-                $q->orWhereHas("interestSync", function ($q1) use ($whereQuery) {
-                    $q1->whereRaw($whereQuery);
-                });
-                //categories
-                $q->orWhereHas("category", function ($q1) use ($whereQuery) {
-                    $q1->whereRaw($whereQuery);
-                    $q1->orWhereHas("parentCategory", function ($q2) use ($whereQuery) {
-                        $q2->whereRaw($whereQuery);
-                    });
-                });
-                //classification
-                $q->orWhereHas("classification", function ($q1) use ($whereQuery) {
-                    $q1->whereRaw($whereQuery);
+            $q->whereHas("occasionSync", function ($q1) use ($whereQuery) {
+                $q1->whereRaw($whereQuery);
+            });
+            // interests
+            $q->orWhereHas("interestSync", function ($q1) use ($whereQuery) {
+                $q1->whereRaw($whereQuery);
+            });
+            //categories
+            $q->orWhereHas("category", function ($q1) use ($whereQuery) {
+                $q1->whereRaw($whereQuery);
+                $q1->orWhereHas("parentCategory", function ($q2) use ($whereQuery) {
+                    $q2->whereRaw($whereQuery);
                 });
             });
-            $qe1->orWhere(function ($q1) use ($itemQuery) {
-                $q1->whereRaw($itemQuery);
+            //classification
+            $q->orWhereHas("classification", function ($q1) use ($whereQuery) {
+                $q1->whereRaw($whereQuery);
             });
+        })->orWhere(function ($q1) use ($itemQuery) {
+            $q1->whereRaw($itemQuery);
         });
         $related = false;
         $newWords = $words;
@@ -380,6 +377,7 @@ class CatalogItem extends Model
                     $query->orWhere('parent_item_id', '0');
                 });
                 $q1->whereRaw("LOWER(name) LIKE '%" . strtolower($text) . "%'");
+                $q1->whereNull('parent_item_id');
             });
         }
 
