04632c9a7db79b864bf8e8c03c0632cdbd0f9af9
mohammad <mohammad@olahub.com>
2020-11-02 21:01:15 +0200

    admin post

---
+++ src/Modules/Posts/Models/postsStatistics.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace OlaHub\UserPortal\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class postsStatistics extends Model
+{
+
+    protected $table = 'admin_posts_statistics';
+
+   
+    protected static function boot()
+    {
+        parent::boot();
+
+    }
+
+ 
+}
